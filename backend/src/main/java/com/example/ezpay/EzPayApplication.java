a085b056d9120f6400373e11b16e4a9dc2d0fc9d
bitofsyn <soyounjeong6276@gmail.com>
2025-12-06 17:22:42 +0900

    no message

--- backend/src/main/java/com/example/ezpay/EzPayApplication.java
+++ backend/src/main/java/com/example/ezpay/EzPayApplication.java
@@ -2,8 +2,12 @@ package com.example.ezpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.example.ezpay")
+@EnableJpaRepositories("com.example.ezpay.*")
 public class EzPayApplication {
 
     public static void main(String[] args) {
